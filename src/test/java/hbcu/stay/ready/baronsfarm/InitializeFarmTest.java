3cf3bae09a7fffd19d01ab27aed9a2f139b36687
stainlessray <raymondcoolmail@gmail.com>
2020-12-13 14:36:26 -0500

    moved farm initialization outside the main class.

--- src/test/java/hbcu/stay/ready/baronsfarm/InitializeFarmTest.java
+++ src/test/java/hbcu/stay/ready/baronsfarm/InitializeFarmTest.java
@@ -3,7 +3,7 @@ package hbcu.stay.ready.baronsfarm;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FarmTest {
+public class InitializeFarmTest {
 
     @Test
     public void testFarmConstructor() {
@@ -13,38 +13,38 @@ public class FarmTest {
 
     @Test
     public void testConfirmFarmhouseFarmerWasCreated() {
-        String actual =  new Farm().getFarmHouse();
+        String actual =  new InitializeFarm().getFarmHouse();
         Assert.assertTrue(actual.contains("Farmer"));
     }
 
     @Test
     public void testConfirmFarmhousePilotWasCreated() {
-        String actual = new Farm().getFarmHouse();
+        String actual = new InitializeFarm().getFarmHouse();
         Assert.assertTrue(actual.contains("Pilot"));
     }
 
     @Test
     public void testGetStables() {
-        String actual = new Farm().getStables();
+        String actual = new InitializeFarm().getStables();
         Assert.assertTrue(actual.contains("stable3"));
     }
 
     @Test
     public void testGetCoups() {
-        String actual = new Farm().getCoups();
+        String actual = new InitializeFarm().getCoups();
         Assert.assertTrue(actual.contains("coup4"));
     }
 
     @Test
     public void testGetFieldOfCrops() {
-        String crops = new Farm().getFieldOfCrops();
+        String crops = new InitializeFarm().getFieldOfCrops();
         System.out.println(crops);
         Assert.assertTrue(crops.contains("Cornstalk"));
     }
 
     @Test
     public void testFertilizeFieldOfCrops() {
-        Farm farm = new Farm();
+        InitializeFarm farm = new InitializeFarm();
         String actual = farm.fertilizeFieldOfCrops();
         System.out.println(actual);
     }
